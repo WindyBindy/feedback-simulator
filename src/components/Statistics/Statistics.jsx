277ae33bbb7936cac3a95d0e76d4a3cc8f98b00c
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2026-05-17 09:25:12 +0200

    default state fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,6 +9,10 @@ render(){
   return (
     <div>
     <h2>Statistics</h2>
+      {total == 0 && 
+      <p>No feedback given</p>
+      }
+      {total !== 0 && 
       <ul>
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
@@ -16,6 +20,7 @@ render(){
         <li>Total:{total} </li>
         <li>Positive feedback: {percantage}%</li>
       </ul>
+      }
     </div>
   )
 }
